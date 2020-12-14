8e3b55a507383e0bbb922d64b46fd76a7a3adc5a
Johnny McClorey <johnnymcclorey@googlemail.com>
2020-12-14 14:25:02 +0000

    Rounding up total fuel number and adding extra lap of fuel for safer fuel calculation

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function calculateFuel() {
 
     lapsPerRace = secondsPerRace/secondsPerLap;
 
-    totalFuel = lapsPerRace * fuelPerLap;
+    totalFuel = lapsPerRace * fuelPerLap + parseInt(fuelPerLap);
 
-    document.getElementById("total-fuel-result").innerHTML = totalFuel.toFixed(2) + " litres";
+    document.getElementById("total-fuel-result").innerHTML = Math.round(totalFuel) + " litres";
 }

31420dacda83657d247ae90068951d3e33f1db4f
Johnny McClorey <johnnymcclorey@googlemail.com>
2020-12-14 13:08:55 +0000

    Adding Initial Calculator Logic
    Initial logic added to script.js to calculate fuel needed according to values given by user

---
+++ script.js
@@ -0,0 +1,18 @@
+function calculateFuel() {
+
+    var raceLength = document.getElementById('race-length').value,
+        lapTimeMinutes = document.getElementById('lap-minutes').value,
+        lapTimeSeconds = document.getElementById('lap-seconds').value,
+        fuelPerLap = document.getElementById('fuel-per-lap').value,
+        secondsPerLap, secondsPerRace, lapsPerRace, totalFuel;
+    
+    
+    secondsPerLap = lapTimeMinutes * 60 + parseInt(lapTimeSeconds);
+    secondsPerRace = raceLength * 60;
+
+    lapsPerRace = secondsPerRace/secondsPerLap;
+
+    totalFuel = lapsPerRace * fuelPerLap;
+
+    console.log(totalFuel);
+}

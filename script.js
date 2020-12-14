9ed7353350ab76298456a8696250c2b688723ced
Johnny McClorey <johnnymcclorey@googlemail.com>
2020-12-14 15:10:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ function calculateFuel() {
         fuelPerLap = document.getElementById('fuel-per-lap').value,
         secondsPerLap, secondsPerRace, lapsPerRace, totalFuel;
     
-    
     secondsPerLap = lapTimeMinutes * 60 + parseInt(lapTimeSeconds);
     secondsPerRace = raceLength * 60;
 
     lapsPerRace = secondsPerRace/secondsPerLap;
 
     totalFuel = lapsPerRace * fuelPerLap + parseInt(fuelPerLap);
-    
 
     document.getElementById("total-fuel-result").innerHTML = Math.round(totalFuel) + " litres";
 }
